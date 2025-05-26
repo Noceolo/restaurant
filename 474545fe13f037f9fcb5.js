01f199f0dc60719ea706f0352cf546f8a5557b4f
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-05-26 16:08:51 +0200

    Deployment commit

---
+++ 474545fe13f037f9fcb5.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+import loadPage from "./loadpage";
+loadPage();
+
+import openTab from "./opentab";
+window.openTab = openTab ;
+
+
+console.log("Odeliott's Restaurant")
+
+
+
+
