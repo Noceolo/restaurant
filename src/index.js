4290f95fbbd9172dcc95c4df4dc6f328f69d8d18
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-05-26 16:00:37 +0200

    Fixed the loadpage error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import "./styles.css";
+
+import loadPage from "./loadpage";
+loadPage();
+
 import openTab from "./opentab";
 window.openTab = openTab ;
+
 
 console.log("Odeliott's Restaurant")
 
